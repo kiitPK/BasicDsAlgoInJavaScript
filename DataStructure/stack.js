fa03aa2ad194dcb55316bd508f59193c0780f5eb
kiitPK <prakashsingh184@gmail.com>
2022-06-27 09:12:35 +0530

    stack code

---
+++ DataStructure/stack.js
@@ -0,0 +1,13 @@
+/* 
+Browser History Tracking
+Undo operation when typing
+Expression Conversion
+Call stack in JavaScript Runtime
+Operation : 
+* Push():
+* Pop(): 
+* peek():
+* isEmpty():
+* size():
+* print():
+*/
