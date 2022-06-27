3e9a7cd4cf37b2d55168ba7191a4423afa1ac9ec
kiitPK <prakashsingh184@gmail.com>
2022-06-27 10:12:06 +0530

    stack code full implementation

--- DataStructure/stack.js
+++ DataStructure/stack.js
@@ -11,3 +11,44 @@ Operation :
 * size():
 * print():
 */
+
+class Stack {
+  constructor() {
+    this.item = [];
+  }
+
+  push(element) {
+    return this.item.push(element);
+  }
+
+  pop() {
+    return this.item.pop();
+  }
+
+  peek() {
+    return this.item[this.item.length - 1];
+  }
+
+  isEmpty() {
+    return this.item.length === 0;
+  }
+
+  size() {
+    return this.item.length;
+  }
+
+  print() {
+    console.log(this.item.toString());
+  }
+}
+
+const stack = new Stack();
+
+stack.print();
+stack.push(50);
+stack.push(150);
+stack.push(110);
+stack.print();
+console.log(stack.size());
+console.log(stack.pop());
+console.log(stack.peek());
